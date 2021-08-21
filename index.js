a8664156afe6d82c493771dcabcbea6c28614367
Raghuram <raghu17122001@gmail.com>
2021-08-22 01:36:52 +0530

    content editable dynamically

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let globalStore = [];
 const newCard = ({id, imageUrl, taskTitle, taskType, taskDescription}) => `<div class="col-md-6 col-lg-4" id=${id}>
 <div class="card ">
   <div class="card-header d-flex justify-content-end gap-2">
-    <button type="button" class="btn btn-outline-success">
-      <i class="fas fa-pencil-alt"></i>
+    <button type="button" id=${id} class="btn btn-outline-success" onclick="editCard.apply(this, arguments)">
+      <i class="fas fa-pencil-alt" id=${id} class="btn btn-outline-success" onclick="editCard.apply(this, arguments)"></i>
     </button>
     <button type="button"  id=${id} class="btn btn-outline-danger"
     onclick="deleteCard.apply(this, arguments)" >
@@ -91,5 +91,32 @@ const deleteCard = (event) => {
 return taskContainer.removeChild(
     event.target.parentNode.parentNode.parentNode.parentNode
 );
+
+};
+
+const editCard = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagname;
+  
+  let parentElement;
+
+  if(tagname == "BUTTON") {
+    parentElement = event.target.parentNode.parentNode;
+  }
+  else { 
+    parentElement = event.target.parentNode.parentNode.parentNode;
+  }
+
+  
+  let taskTitle = parentElement.childNodes[5].childNodes[1];
+  let taskDescription = parentElement.childNodes[5].childNodes[3];
+  let taskType = parentElement.childNodes[5].childNodes[5];
+  let submitButton = parentElement.childNodes[7].childNodes[1];
+
+  taskTitle.setAttribute("contenteditable", "true");
+  taskType.setAttribute("contenteditable", "true");
+  taskDescription.setAttribute("contenteditable", "true");
+  submitButton.innerHTML ="Save Changes";
 
 };
